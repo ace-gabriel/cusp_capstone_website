1791de9b3b18752c84dc6f1178127ba47af45d49
ace-gabriel <gabrielyin@berkeley.edu>
2019-03-02 13:59:53 -0500

    beta test

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+from resp import parse
+app = Flask(__name__)
+
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template("main.html")
+
+
+@app.route("/result", methods=['GET', 'POST'])
+def main():
+    req = request.form
+    first, last, age = req.get("first_name"), req.get("last_name"), req.get("age")
+    req = {"full_name": parse(first, last, age), "age": age}
+    return render_template("result.html", result=req)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
